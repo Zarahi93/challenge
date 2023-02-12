e2d599bdb49bd646b5ca0062bda3b5f0db196e7f
Zarahi93 <107142458+Zarahi93@users.noreply.github.com>
2023-02-12 14:34:02 -0600

    Trying to listing all resources to the app

--- src/components/postList.js
+++ src/components/postList.js
@@ -2,10 +2,22 @@ import React from "react";
 import "./postlist.css";
 
 export default function PostList() {
-    const posts = [{ title: 'Post 1', body: "13123" }, { title: 'Post 2', body: "34534" }, { title: 'Post 3', body: "jfddcvbhjbbyuijn, cfvygbhnjkxdcfvgh" }, { title: ' Post 4', body: "45646765" },]
+   const getPosts = async() =>{
+    try{
+    const resp = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const allPosts = await resp.json();
+    return allPosts;
+    } catch (error) {
+     console.error(error);
+    }
+   };
+
+   getPosts();
+   console.log(getPosts);
+    const postss = [{ title: 'Post 1', body: "13123" }, { title: 'Post 2', body: "34534" }, { title: 'Post 3', body: "jfddcvbhjbbyuijn, cfvygbhnjkxdcfvgh" }, { title: ' Post 4', body: "45646765" },]
     return (
           <section className="post-list">
-          { posts.map((post) => <article className="post" key={post.id} style={{ backgroundImage: `url("https://source.unsplash.com/random")`}}>
+          { postss.map((post) => <article className="post" key={post.id} style={{ backgroundImage: `url("https://source.unsplash.com/random")`}}>
           <p className="title"> {post.title} </p>
           <p className="body">{post.body}</p>
         </article>)}
