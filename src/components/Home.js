f28d89f8d0f0ab61520b47827eac800921b34d03
Zarahi93 <107142458+Zarahi93@users.noreply.github.com>
2023-02-12 10:08:52 -0600

    Creating the Home component and inst React Router

---
+++ src/components/Home.js
@@ -0,0 +1,22 @@
+//import { useNavigate } from 'react-router-dom';
+
+export default function Home() {
+  //const navigate = useNavigate();
+
+//   const goToNewNote = () => {
+//     navigate('/notes');
+//   };
+  return (
+<main id="mainpage">
+    <header id="header">
+    <h3 id="slogan">[Making your Life Easier] </h3>
+    <h1 id="maintitle">Discovering the world</h1>
+    </header>
+    <section id='mainsection'>
+    </section>
+    <footer id='mainfooter'>
+        <button  id='log-out'></button>
+    </footer>
+</main>
+  );
+}
